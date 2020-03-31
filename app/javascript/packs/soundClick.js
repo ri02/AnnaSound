a02c5acabe03b32d8fc41691e12d658b1e255dc9
Marie Hess <hess.marietheres@gmail.com>
2020-03-31 13:45:38 +0200

    stop sound when new sound is clicked

--- app/javascript/packs/soundClick.js
+++ app/javascript/packs/soundClick.js
@@ -4,6 +4,11 @@ const playSound = () => {
   const sound = document.querySelectorAll('.sound');
   sound.forEach ((s) => {
     s.addEventListener("click", (event) => {
+      const sounds = document.querySelectorAll(".snipet");
+      sounds.forEach ((music) => {
+        music.pause();
+        music.currentTime = 0;
+        });
       const soundOne = event.currentTarget.querySelector(".snipet");
       soundOne.play();
     });
